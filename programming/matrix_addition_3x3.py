cabe1e80aaecb85f81631f885f55417e3a915887
Mohd Ajlal <ajlal00786@gmail.com>
2022-11-29 02:12:46 +0530

    PYTHON

--- programming/matrix_addition_3x3.py
+++ programming/matrix_addition_3x3.py
@@ -1,15 +1,17 @@
 # wap in python to take two 3x3 matrix and add them
 
 l1 = []
+a = []
 print('Enter matrix 1: ')
-for i in range(1,4):
-    m1 = list(map(int, input().split()))
-    m1.append(l1)
+for i in range(0,3):
+    l1 = list(map(int, input().split()))
+    a.append(l1)
 l2 = []
+b = []
 print('Enter matrix 2: ')
-for i in range(1,4):
-    m2 = list(map(int, input().split()))
-    m2.append(l2)
+for i in range(0,3):
+    l2 = list(map(int, input().split()))
+    b.append(l2)
 c = []
 for i in range(0,3):
     c1 = [0,0,0]
