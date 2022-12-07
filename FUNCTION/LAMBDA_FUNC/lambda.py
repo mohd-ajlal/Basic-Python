20add18740eea8eff7b1ce1396b15b37180cce04
Mohd Ajlal <ajlal00786@gmail.com>
2022-12-08 01:25:07 +0530

    Python

---
+++ FUNCTION/LAMBDA_FUNC/lambda.py
@@ -0,0 +1,10 @@
+# Python Lambda Functions are anonymous function means that the function is without a name.
+# As we already know that the def keyword is used to define a normal function in Python.
+# Similarly, the lambda keyword is used to define an anonymous function in Python.
+# It is a small and restricted function having no more than one line. 
+# Just like a normal function, a Lambda function can have multiple arguments with one expression.
+# The expression is evaluated and returned.
+# Syntax: lambda arguments: expression
+# Example: lambda x: x + 10
+# The above function takes one argument and adds 10 to it.
+# Lambda functions are used along with built-in functions like filter(), map() etc.
