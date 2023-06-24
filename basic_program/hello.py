82ca05ba57f2a9bfb238c9bf37c90d01dcd729dc
Mohd Ajlal <ajlal00786@gmail.com>
2023-06-25 03:55:28 +0530

    Python

--- basic_program/hello.py
+++ basic_program/hello.py
@@ -1,1 +1,23 @@
-print("Hello world!")
+# print("Hello world!") #to print anything in python we use print() function
+
+# "" - string
+# '' - string
+# '''''' - string
+
+# print("Hello world!")
+# print('Hello')
+# print('''hello
+# world hjed
+# hbwds
+# uidewjdk
+# hbkjbdw
+# uwdejh''')
+
+
+'''hello 
+world
+hello'''
+
+print("hello")
+# \n - new line
+print("world")
